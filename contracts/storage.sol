3f5331ecdd7f411db3d2b9bce357750dfba8d1d8
jmeis <jmeis@users.noreply.github.com>
2016-03-01 23:57:44 -0600

    Removed comments.

--- contracts/storage.sol
+++ contracts/storage.sol
@@ -1,13 +1,10 @@
 contract SimpleStorage {
-  // parameters
   uint storedData;
 
-  // sets value
   function set(uint x) {
     storedData = x;
   }
 
-  // gets value
   function get() constant returns (uint retVal) {
     return storedData;
   }
